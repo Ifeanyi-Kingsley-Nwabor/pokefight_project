091d802d1ac1175ad8552b91c147a15b844f2b11
Codify KING <81686044+Ifeanyi-Kingsley-Nwabor@users.noreply.github.com>
2021-11-06 00:30:08 +0100

    add complet part 1

---
+++ controllers/pokefightControllers.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const data = require('../file.json');
+
+//const Game = require('../models/Game');
+
+const allPokemon = (req, res, next) => {
+    res.json(data)
+    }
+
+const onePokemon = async (req, res, next) => {
+    const { id } = req.params;
+    const getPokemon = await data.pokemonData.find((pokemon) => pokemon.id === Number(id));
+    res.json(getPokemon)
+}
+
+const pokemonInfo = (req, res, next) => {
+    const { id, info } = req.params;
+    const getPokemon = data.pokemonData.find((pokemon) => pokemon.id === +id);// the + sign is the same as converting string to number
+    const getInfo = Object.keys(getPokemon).find((pokemon) => pokemon === info)
+    res.json(getPokemon[getInfo])
+}
+
+module.exports = {
+    allPokemon,
+    onePokemon,
+    pokemonInfo
+}
